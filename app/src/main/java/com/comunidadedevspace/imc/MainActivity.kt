b30ee907db293a7fc3a6afe4c59d8af23c79a3ab
pedrosalmeron <psalmeronsilva@gmail.com>
2024-03-20 18:54:37 -0300

    Calculando o IMC

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity() {
             val resultado : Float = peso / alturaq2
 
 
-            println("Pedro " + resultado)
+            println("Peter " + resultado)
         }
 
 
